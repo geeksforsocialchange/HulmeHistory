8299096c218d8455ee1fbfd4e8e60ae78ec3f0a6
Kim Foale <kim@alliscalm.net>
2015-08-08 21:01:17 +0100

    Start ghpages

--- config.rb
+++ config.rb
@@ -35,6 +35,15 @@
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
+# Use github pages
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = 'master'
+  deploy.build_before = true
+end
+
+activate :directory_indexes
+
 # Reload the browser automatically whenever files change
 # configure :development do
 #   activate :livereload
