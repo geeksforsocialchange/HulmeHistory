aec53d588b6bf59e64f8e0e862291672889aebe5
Kim Foale <kim@alliscalm.net>
2015-09-15 13:20:37 +0100

    Add archive catalog

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,8 @@
 //= require vendor/jquery/dist/jquery.min
 //= require vendor/mapbox.js/mapbox
+//= require vendor/foundation/js/foundation.min
+//= require vendor/datatables/media/js/jquery.dataTables.min
+//= require vendor/datatables/media/js/dataTables.foundation.min
 //= require timeline
 //= require mapcontroller
 //= require overlay
